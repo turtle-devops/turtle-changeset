91893807418d5d5d0dc624838980240e9fba788c
Thanh huy <adjustmode1@gmail.com>
2025-07-25 17:02:30 +0700

    feat: init packages

---
+++ apps/apis/dist/index.js
@@ -0,0 +1,2 @@
+console.log('This is server api');
+//# sourceMappingURL=index.js.map
---
+++ packages/utils/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = sum;
+function sum(a, b) {
+    return a + b;
+}
+//# sourceMappingURL=index.js.map
